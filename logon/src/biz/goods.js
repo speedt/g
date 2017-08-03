ffcc4076f801cbefe51647b8e4f46fbfa4420c95
3203317 <3203317@qq.com>
2017-08-03 11:57:56 +0800

    haha

--- logon/src/biz/goods.js
+++ logon/src/biz/goods.js
@@ -30,3 +30,88 @@ const _ = require('underscore');
     });
   };
 })();
+
+(() => {
+  const sql = 'INSERT INTO w_goods (id, goods_name, goods_desc, create_time, game_currency, cost, payment_id, disposable, interval_time) values (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  /**
+   *
+   * @return
+   */
+  exports.saveNew = function(newInfo, cb){
+
+    var postData = [
+      utils.replaceAll(uuid.v1(), '-', ''),
+      newInfo.goods_name,
+      newInfo.goods_desc,
+      new Date(),
+      newInfo.game_currency,
+      newInfo.cost,
+      newInfo.payment_id,
+      newInfo.disposable,
+      newInfo.interval_time,
+    ];
+
+    mysql.query(sql, postData, function (err, status){
+      if(err) return cb(err);
+      cb(null, status);
+    });
+  };
+})();
+
+(() => {
+  const sql = 'UPDATE w_goods SET goods_name=?, goods_desc=?, game_currency=?, cost=?, payment_id=?, disposable=?, interval_time=? WHERE id=?';
+
+  /**
+   *
+   * @return
+   */
+  exports.saveInfo = function(newInfo, cb){
+
+    var postData = [
+      newInfo.goods_name,
+      newInfo.goods_desc,
+      newInfo.game_currency,
+      newInfo.cost,
+      newInfo.payment_id,
+      newInfo.disposable,
+      newInfo.interval_time,
+      newInfo.id,
+    ];
+
+    mysql.query(sql, postData, function (err, status){
+      if(err) return cb(err);
+      cb(null, status);
+    });
+  };
+})();
+
+(() => {
+  var sql = 'SELECT a.* FROM w_goods a WHERE a.id=?';
+
+  /**
+   *
+   * @return
+   */
+  exports.getById = function(id, cb){
+    mysql.query(sql, [id], (err, docs) => {
+      if(err) return cb(err);
+      cb(null, mysql.checkOnly(docs) ? docs[0] : null);
+    });
+  };
+})();
+
+(() => {
+  var sql = 'DELETE FROM w_goods WHERE id=?';
+
+  /**
+   *
+   * @return
+   */
+  exports.del = function(id, cb){
+    mysql.query(sql, [id], (err, status) => {
+      if(err) return cb(err);
+        cb(null, status);
+    });
+  };
+})();
