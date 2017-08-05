446a361fefd65db639cb5b53df4ac2fa6331d054
speedt <huangxin@foreworld.net>
2017-08-05 19:07:17 +0800

    haha

--- logon/src/biz/payment.js
+++ logon/src/biz/payment.js
@@ -43,38 +43,30 @@ const goods = require('./goods');
     }, conn);
   }
 
-  // function step3(payInfo, conn, resolve, reject){
+  function step3(payInfo, conn, resolve, reject){
 
-  //   user.updateVip(payInfo.user_id, function (err, status){
-  //     if(err) return reject(err);
-  //     resolve();
-  //   }, conn);
-  // }
+    user.updateVip(payInfo.user_id, function (err, status){
+      if(err) return reject(err);
+      resolve();
+    }, conn);
+  }
 
-  // function step4(payInfo, conn, resolve, reject){
+  function step4(payInfo, conn, resolve, reject){
 
-  //   goods.getById(payInfo.goods_id, function (err, doc){
-  //     if(err) return reject(err);
-  //     if(!doc) return reject('11');
-  //     resolve(doc);
-  //   }, conn);
-  // }
+    goods.getById(payInfo.goods_id, function (err, doc){
+      if(err) return reject(err);
+      if(!doc) return reject('11');
+      resolve(doc);
+    }, conn);
+  }
 
-  // function step5(payInfo, conn, resolve, reject){
+  function step5(payInfo, conn, goods_info, resolve, reject){
 
-  //   user.updatePaymentToCache(payInfo.goods_id, function (err, doc){
-  //     if(err) return reject(err);
-  //     resolve(doc);
-  //   }, conn);
-  // }
-
-  // function step6(payInfo, conn, goods_info, resolve, reject){
-
-  //   user.updatePaymentToCache(payInfo.goods_id, function (err, doc){
-  //     if(err) return reject(err);
-  //     resolve(doc);
-  //   }, conn);
-  // }
+    user.updatePaymentToCache(payInfo.goods_id, function (err, doc){
+      if(err) return reject(err);
+      resolve(doc);
+    }, conn);
+  }
 
   var private_key  = 'E2D5511AFC845DDF8CE220ACE2A0A1C9';
   var enhanced_key = 'OWE2ZmMyOGVmMWNhYzc0MmYyOWU';
@@ -142,15 +134,12 @@ const goods = require('./goods');
 
         new Promise(step1.bind(null, payInfo, conn)).then(function(){
           return new Promise(step2.bind(null, payInfo, conn));
+        }).then(function (order_info){
+          return new Promise(step3.bind(null, payInfo, conn));
         }).then(function(){
-
-          conn.commit(function (err){
-            if(err) return conn.rollback(function(){ cb(err); });
-
-
-            
-          });
-
+          return new Promise(step4.bind(null, payInfo, conn));
+        }).then(function (goods_info){
+          return new Promise(step5.bind(null, payInfo, conn, goods_info));
         }).catch(err => {
           conn.rollback(function(){
             if('object' === typeof err) return cb(err);
@@ -160,6 +149,32 @@ const goods = require('./goods');
 
       });
     });
+
+
+
+
+
+
+
+    // user.updatePurchase(payInfo.user_id, payInfo.amount, function (err, status){
+    //   if(err) return cb(err);
+    //   if(0 === status.changedRows) return;
+
+    //   user_purchase.saveNew({ goods_id: payInfo.product_id, user_id: payInfo.user_id }, function (err, status){
+    //     if(err) return cb(err);
+
+    //     user.updateVip(payInfo.user_id, function (err, status){
+    //       if(err) return cb(err);
+
+    //       user.getById(payInfo.user_id, function (err, doc){
+    //         if(err) return cb(err);
+    //         cb(null, doc);
+    //       });
+    //     });
+
+    //   });
+
+    // });
 
   };
 })();
